9d55d6ecf759067da173f4461e3b12c866966038
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2023-08-28 00:08:21 +0800

    Update GameOverSubstate.hx

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -27,7 +27,7 @@ class GameOverSubstate extends MusicBeatSubstate
 	public static var deathSoundName:String = 'fnf_loss_sfx';
 	public static var loopSoundName:String = 'gameOver';
 	public static var endSoundName:String = 'gameOverEnd';
-
+        public static var vibrationTime:Int = 500; 
 	public static var instance:GameOverSubstate;
 
 	public static function resetVariables() {
