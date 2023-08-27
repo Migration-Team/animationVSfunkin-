99267bd2858ff562edbfa679b4d80240f6cd7e47
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2023-08-27 18:52:33 +0800

    Update FlashingState.hx

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -24,13 +24,25 @@ class FlashingState extends MusicBeatState
 		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		add(bg);
 
+		#if android
 		warnText = new FlxText(0, 0, FlxG.width,
-			"Hey there person man/woman   \n
-			Some songs contains some flashing and it may cause a headache,\n
-			Press B if you want to disable it or press A if you don't wanna disable it,\n
-			\n
-			Hope you enjoy this mod.",
+			"Hey, watch out!\n
+			Be careful when you touch the phone fast!\n
+			You can break your phone screen if you do that,also\n
+			This Mod contains some flashing lights!\n
+			Press A to disable them now or go to Options Menu.\n
+			Press B to ignore this message.\n
+			You've been warned!",
 			32);
+		#else
+		warnText = new FlxText(0, 0, FlxG.width,
+			"Hey, watch out!\n
+			This Mod contains some flashing lights!\n
+			Press ENTER to disable them now or go to Options Menu.\n
+			Press ESCAPE to ignore this message.\n
+			You've been warned!",
+			32);
+		#end
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
 		add(warnText);
