2bd7016302375f3bf85ce5daad954ec968c5a753
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2023-08-28 14:55:10 +0800

    Update GameplayChangersSubstate.hx

--- source/GameplayChangersSubstate.hx
+++ source/GameplayChangersSubstate.hx
@@ -134,6 +134,10 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 
 		changeSelection();
 		reloadCheckboxes();
+	
+	#if android
+	addVirtualPad(FULL, A_B);
+	#end
 	}
 
 	var nextAccept:Int = 5;
